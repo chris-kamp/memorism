d45405e137db977ca978e4bdbbe69eebb9efb63c
Chris Kamp <chriskamp101@gmail.com>
2021-06-20 21:51:58 +1000

    display login, signup and sign out links in nav consistent with user state

--- app/javascript/components/topnav/Topnav.js
+++ app/javascript/components/topnav/Topnav.js
@@ -4,8 +4,10 @@ import Nav from "react-bootstrap/Nav"
 import { LinkContainer } from 'react-router-bootstrap'
 
 const Topnav = ({user: userObj}) => {
-  const [user, setUser] = useState("null");
+  // Initialise "user" state
+  const [user, setUser] = useState(null);
 
+  // Parse Rails user object from JSON
   useEffect(() => {
     setUser(JSON.parse(userObj));
   }, userObj)
@@ -14,16 +16,21 @@ const Topnav = ({user: userObj}) => {
     <>
       <Navbar expand="lg" bg="dark" variant="dark">
         <LinkContainer to="/decks"><Navbar.Brand>Memorism</Navbar.Brand></LinkContainer>
+        {/* Wraps elements which collapse into mobile dropdown menu */}
         <Navbar.Collapse id="responsive-navbar-nav">
+          {/* Left-aligned nav elements */}
           <Nav className="mr-auto">
             <LinkContainer to="/decks"><Nav.Link>Home</Nav.Link></LinkContainer>
-            <Nav.Link href="/users/sign_in">Log In</Nav.Link>
+            {!user && (<Nav.Link href="/users/sign_in">Log In</Nav.Link>)}
+            {!user && (<Nav.Link href="/users/sign_up">Sign Up</Nav.Link>)}
           </Nav>
+          {/* Right-aligned nav elements */}
           <Nav>
-            <Nav.Link href="/users/sign_out" data-method="delete">Log Out</Nav.Link>
+            {user && (<Nav.Link href="/users/sign_out" data-method="delete">Log Out</Nav.Link>)}
           </Nav>
         </Navbar.Collapse>
-        <Navbar.Text className="m-auto">{user === null ? "" : user.username}</Navbar.Text>
+        {/* Display user's username to right of nav, or in center of mobile dropdown */}
+        {user && (<Navbar.Text className="m-auto">{user.username}</Navbar.Text>)}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       </Navbar>
     </>
